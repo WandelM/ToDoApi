e7a7f9c0452582a976e6ad29e21e585c16719f55
Marcin Wandel <wandelmarcin@gmail.com>
2022-10-05 06:30:45 +0200

    Added appsettings custom configuration

--- ToDo.API/ToDo.API/Program.cs
+++ ToDo.API/ToDo.API/Program.cs
@@ -6,6 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Host.ConfigureAppConfiguration((context, config) =>
+{
+    config.Sources.Clear();
+
+    config.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true);
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
